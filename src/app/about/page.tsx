5a76b35620d328c25b7d49f8b0bda1341c38f3a4
dmytro_shchepinskyi <Dmytro.Shchepinskyi@mheducation.com>
2023-10-23 09:58:03 +0200

    about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div className="w-3/4 text-center flex flex-col gap-3">
+      <p>
+        Hello, I'm Dmytro, born on April 6, 1991, and I'm thrilled to have you
+        visit my personal website. I'm more than just a full-stack developer;
+        I'm a passionate creator of digital experiences with over a decade of
+        expertise in this dynamic field.
+      </p>
+      <h3 className="text-3xl">About Me:</h3>{" "}
+      <p>
+        With a strong foundation in web development, I've had the privilege of
+        watching the internet evolve over the years. My journey as a developer
+        has been a thrilling ride, marked by constant learning and growth. From
+        the early days of HTML and CSS to the complex world of modern web
+        applications, I've embraced every challenge and change in this
+        ever-evolving industry. My goal is to craft elegant, efficient, and
+        user-friendly solutions that empower businesses and individuals.
+      </p>{" "}
+      <h3 className="text-3xl">Beyond Coding:</h3>
+      <p>
+        {" "}
+        While coding is my profession, there's more to me than just lines of
+        code. When I'm not immersed in the digital realm, you can find me
+        pursuing two of my greatest passions:
+      </p>{" "}
+      <p>
+        Gym Enthusiast: Fitness is not just a hobby for me; it's a way of life.
+        The gym is where I find my balance, push my limits, and stay physically
+        and mentally fit. The dedication I bring to the world of coding is
+        mirrored in my approach to maintaining a healthy lifestyle. Motorcycle
+        Traveler: I'm a wanderer at heart, and there's no better way to explore
+        the world than on two wheels. The freedom of the open road, the wind in
+        my face, and the thrill of discovering new places are what fuel my
+        motorcycle adventures. I've ridden through breathtaking landscapes, met
+        fascinating people, and created memories that will last a lifetime.
+      </p>{" "}
+      <h3 className="text-3xl">Get in Touch:</h3>
+      <p>
+        I love connecting with fellow developers, fitness enthusiasts, and
+        adventurers. Whether you want to discuss coding, share travel stories,
+        or exchange fitness tips, I'm just a message away. Feel free to reach
+        out via the contact form or connect with me on social media. Thank you
+        for visiting my website and taking a glimpse into my multifaceted world.
+        I look forward to sharing more with you as we embark on this journey
+        together.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
